2405eb7d7083a74896c313f898fdf6ff178d972a
tonakihan <116086641+tonakihan@users.noreply.github.com>
2024-09-27 15:25:48 +0900

    i3wm: polybar addition tray & update autostart

--- WM/i3wm/i3/autostart.sh
+++ WM/i3wm/i3/autostart.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-udiskie -aN &disown
-corectrl &disown
-dunst &disown
+udiskie -aN &
+corectrl --minimize-systray &
+dunst &
 
 # Для того, чтобы не уходил в спячку экран
 xset s off; xset dpms 0 0 0 
 
+# Отключение второго монитора
+xrandr --output DisplayPort-1 --off
+# На проектор
+xrandr --output DisplayPort-2 --same-as HDMI-A-0
