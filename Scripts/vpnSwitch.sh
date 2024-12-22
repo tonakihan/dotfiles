587723c34357d273fd56242e70c08d2b0c5fc33f
tonakihan <116086641+tonakihan@users.noreply.github.com>
2024-12-22 10:51:35 +0900

    Updated vpnSwitch.sh

--- Scripts/vpnSwitch.sh
+++ Scripts/vpnSwitch.sh
@@ -1,21 +1,55 @@
 #!/usr/bin/env bash
 
-#FIXME: добавить env и заменить в файлах на нужный интерфейс..
-WG_INTERFACE=wg1;
+# WARNING: Needed the Polkit and notify-send!
+# TODO: Add helps
 
-getPassword=$(zenity --password --title "Elevate privileges");
+# FIXME: добавить env в setup и заменить на нужный интерфейс..
+setEnv() {
+  if [ -z $WG_INTERFACE ]; then
+    WG_INTERFACE=wg;
+  fi
+  # У меня проблемы с передачей сообщения от wg-quick, по этому это здесь
+  # FIXME: В waybar опять обрыв канала - файл не получает свой случайный идентификатор
+  TEMP_FILE="/tmp/_sh-$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13; echo)"
+}
 
-main() {
+notifySend() {
+  local selfNameApp=$(basename "$0")
+  notify-send "$selfNameApp" "$(echo $@)"
+}
+
+switchVPN() {
   # Check interface wg is online
   if [ -n "$(ip link show type wireguard)" ]; then
-    echo "$getPassword" | \
-    sudo -S wg-quick down $WG_INTERFACE && \
-    echo "VPN is down";
+    local action="down"
   else
-    echo "$getPassword" | \
-    sudo -S wg-quick up $WG_INTERFACE && \
-    echo "VPN is up";
+    local action="up"
+  fi
+  
+  pkexec wg-quick $action $WG_INTERFACE &> $TEMP_FILE
+  return $?
+}
+
+checkError() {
+  local status=$1
+  local msg=$2
+  if [ $status -ne 0 ]; then
+    case $msg in
+      *"does not exist")
+        notifySend "Error: Please add WG_INTERFACE=your_name_wg_conf" \
+        "before launch command or modify this script." \
+        "Path: \"$0\"";;
+      *)
+        notifySend "Unknown error: $msg";;
+    esac
+    exit 1
   fi
 }
 
+main() {
+  setEnv;
+  switchVPN;
+  checkError $? "$(cat $TEMP_FILE)";
+  rm -f $TEMP_FILE
+}
 main;
